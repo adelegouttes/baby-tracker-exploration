a64f0f9aaef31cd297da34457b26e583d9234dbe
agouttes <adele.gouttes@zalando.de>
2021-01-19 19:36:16 +0100

    Add a mocked command to explore data
    Follow 1st tutorial of Hypermodern python

---
+++ baby_tracker_exploration/console.py
@@ -0,0 +1,21 @@
+import os
+
+import click
+import pandas as pd
+
+
+DATA_PATH = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    "data/BabyRecords.csv"
+)
+
+
+def get_data(data_path=DATA_PATH):
+    df = pd.read_csv(data_path)
+    feeding_df = df.loc[df.RecordCategory == "Feeding"]
+    print(feeding_df.head())
+
+
+@click.command()
+def main():
+    return get_data()
