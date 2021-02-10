5e04593e82bc16c4dd1ec98af428b0c696eada20
agouttes <adele.gouttes@zalando.de>
2021-02-10 09:51:33 +0100

    Call main notebook directly from console

--- baby_tracker_exploration/console.py
+++ baby_tracker_exploration/console.py
@@ -2,8 +2,11 @@ import os
 
 import click
 
+ABS_PATH = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "explorator.ipynb"
+)
 
 @click.command()
 def main():
     os.system("ipython kernel install --user --name=baby-tracker-exploration")
-    os.system("jupyter-notebook . --kernel baby-tracker-exploration ")
+    os.system("jupyter-notebook {path} --kernel baby-tracker-exploration ".format(path=ABS_PATH))
